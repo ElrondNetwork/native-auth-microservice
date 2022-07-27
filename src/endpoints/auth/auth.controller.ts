5e66e49c32ca66aea21b1284f6960f932ac4279f
Traian Anghel <tanghel@live.com>
2022-07-27 12:53:26 +0300

    adjusted auth controller to use native auth guard

--- src/endpoints/auth/auth.controller.ts
+++ src/endpoints/auth/auth.controller.ts
@@ -1,19 +1,19 @@
 import { Controller, Get, UseGuards } from "@nestjs/common";
 import { ApiResponse, ApiTags } from "@nestjs/swagger";
-import { Jwt, JwtAuthenticateGuard } from "@elrondnetwork/erdnest";
+import { NativeAuth, NativeAuthGuard } from "@elrondnetwork/erdnest";
 
 @Controller()
 @ApiTags('auth')
 export class AuthController {
 	@Get("/auth")
-	@UseGuards(JwtAuthenticateGuard)
+	@UseGuards(NativeAuthGuard)
 	@ApiResponse({
 		status: 200,
 		description: 'Authorizes the user and returns the encoded address',
 	})
 	async authorize(
-		@Jwt('address') address: string
-	): Promise<string> {
-		return address;
+		@NativeAuth() auth: unknown
+	): Promise<unknown> {
+		return auth;
 	}
 }
